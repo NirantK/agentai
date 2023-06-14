dc6ef63cee04645ac285404666b20425ff5f0274
NirantK <nirant.bits@gmail.com>
2023-06-14 22:47:34 +0530

    * refactor(api.py): remove unused import and print statements * feat(api.py): add logging using loguru library * feat(api.py): add return_function_params parameter to chat_complete function * fix(api.py): raise ValueError if unexpected message is returned by chat_complete function

--- agentai/api.py
+++ agentai/api.py
@@ -7,19 +7,19 @@ import openai
 import requests
 from tenacity import (
     retry,
-    retry_if_exception,
     retry_if_exception_type,
     stop_after_attempt,
-    wait_random_exponential,
 )
 
 from .conversation import Conversation
 from .openai_function import ToolRegistry
 
+from loguru import logger
+
 
 @retry(retry=retry_if_exception_type(ValueError), stop=stop_after_attempt(3))
 def chat_complete(
-    messages, model, function_registry: ToolRegistry = None, debug: bool = False, return_function: bool = False
+    messages, model, function_registry: ToolRegistry = None, debug: bool = False, return_function_params: bool = False
 ):
     if openai.api_key is None:
         raise ValueError("Please set openai.api_key and try again")
@@ -27,27 +27,25 @@ def chat_complete(
         "Content-Type": "application/json",
         "Authorization": "Bearer " + openai.api_key,
     }
-    if debug:
-        print("Generating ChatCompletion response")
-        print(f"messages: {messages}")
     if not len(messages) > 0:
         raise ValueError("messages must be a list of strings")
     json_data = {"model": model, "messages": messages}
     if function_registry is not None:
         functions = function_registry.get_all_function_information()
+        logger.debug(f"functions: {functions}")
         json_data.update({"functions": functions})
-        if debug:
-            print(f"functions: {functions}")
 
     response = requests.post(
         "https://api.openai.com/v1/chat/completions",
         headers=headers,
         json=json_data,
     )
-    if return_function:
-        if response.json()["choices"][0]["message"]["content"] is not None:
-            raise ValueError(f"OpenAI API returned unexpected output: {response.json()}")
-        return response
+    if return_function_params:
+        message = response.json()["choices"][0]
+        if message["finish_reason"] == "function_call":
+            return response
+        else:
+            raise ValueError(f"Unexpected message: {message}")
     else:
         content = response.json()["choices"][0]["message"]["content"]
         if content is None:
@@ -70,12 +68,11 @@ def get_function_arguments(message, conversation: Conversation, function_registr
             function_arguments = get_function_arguments(
                 message, conversation, function_registry=function_registry, model=model
             )
-    else:
-        print("Function not required, responding to user")
-    return function_arguments
+        return function_arguments
+    raise ValueError(f"Unexpected message: {message}")
 
 
-@retry(retry=retry_if_exception, wait=wait_random_exponential(min=5, max=40), stop=stop_after_attempt(3))
+@retry(retry=retry_if_exception_type(ValueError), stop=stop_after_attempt(3))
 def chat_complete_execute_fn(
     conversation: Conversation,
     function_registry: ToolRegistry,
@@ -84,16 +81,27 @@ def chat_complete_execute_fn(
     debug: bool = False,
 ):
     response = chat_complete(
-        conversation.conversation_history, function_registry=function_registry, model=model, debug=debug
+        conversation.conversation_history,
+        function_registry=function_registry,
+        model=model,
+        debug=debug,
+        return_function_params=True,
     )
-    print(response.json())
     message = response.json()["choices"][0]
     function_arguments = get_function_arguments(
         message=message, conversation=conversation, function_registry=function_registry, model=model
     )
+    logger.debug(f"function_arguments: {function_arguments}")
     results = callable_function(**function_arguments)
+    logger.debug(f"results: {results}")
     conversation.add_message(role="function", name=callable_function.__name__, content=str(results))
-    response = chat_complete(conversation.conversation_history, function_registry=function_registry, model=model)
+    response = chat_complete(
+        conversation.conversation_history,
+        function_registry=function_registry,
+        model=model,
+        return_function_params=False,
+    )
     assistant_message = response.json()["choices"][0]["message"]["content"]
+    logger.debug(f"assistant_message: {assistant_message}")
     conversation.add_message(role="assistant", content=assistant_message)
     return assistant_message
