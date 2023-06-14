1c7bfe9fa94086e5d460a65ca8fb9d4bcc988f16
NirantK <nirant.bits@gmail.com>
2023-06-14 14:37:59 +0530

    * refactor(function_parser.py): simplify the code by removing commented out code and redundant imports * feat(function_parser.py): add support for Enum types in function annotations * feat(function_parser.py): add json_info attribute to function objects

--- agentai/function_parser.py
+++ agentai/function_parser.py
@@ -4,7 +4,9 @@ uses docstring to give a JSON format that can be used by the OpenAI API.
 """
 import inspect
 import typing
-from typing import Any
+import json
+import enum
+from typing import Any, Callable
 
 from docstring_parser import parse
 
@@ -24,10 +26,18 @@ def to_json_schema_type(type_name: str) -> str:
     return type_map.get(type_name, "any")
 
 
+import enum
+import inspect
+from typing import Any
+from docstring_parser import parse
+
+
 def parse_annotation(annotation):
     if getattr(annotation, "__origin__", None) == typing.Union:
         types = [t.__name__ if t.__name__ != "NoneType" else "None" for t in annotation.__args__]
         return to_json_schema_type(types[0])
+    elif issubclass(annotation, enum.Enum):  # If the annotation is an Enum type
+        return "enum", [item.name for item in annotation]  # Return 'enum' and a list of the names of the enum members
     elif getattr(annotation, "__origin__", None) is not None:
         if annotation._name is not None:
             return f"{to_json_schema_type(annotation._name)}[{','.join([to_json_schema_type(i.__name__) for i in annotation.__args__])}]"
@@ -48,20 +58,18 @@ def get_function_info(func: Any) -> str:
     for name, param in signature.parameters.items():
         json_type = parse_annotation(param.annotation)
 
-        param_info = {"type": json_type, "description": ""}
+        if isinstance(json_type, tuple) and json_type[0] == "enum":  # If the type is an Enum
+            param_info = {
+                "type": "string",
+                "enum": json_type[1],  # Add an 'enum' field with the names of the enum members
+                "description": "",
+            }
+        else:
+            param_info = {"type": json_type, "description": ""}
 
-        # Parsing Default Arguments if needed
-        # if param.default != inspect.Parameter.empty:
-        #     if isinstance(param.default, str):
-        #         param_info["default"] = param.default
-        #     else:
-        #         param_info["default"] = str(param.default)
-
-        # If the parameter type is not "any", it's not 'self', and it has no default value, it's required.
         if json_type != "any" and name != "self" and param.default == inspect.Parameter.empty:
             required.append(name)
 
-        # Extract description from parsed docstring
         for doc_param in docstring_parsed.params:
             if doc_param.arg_name == name:
                 param_info["description"] = doc_param.description
@@ -71,10 +79,13 @@ def get_function_info(func: Any) -> str:
     function_info = {
         "name": func.__name__,
         "description": docstring_parsed.short_description,
-        "parameters": {
-            "type": "object",
-            "properties": parameters,
-        },
-        "required": required,
+        "parameters": {"type": "object", "properties": parameters, "required": required},
     }
-    return function_info
+
+    return json.dumps(function_info, indent=4)
+
+
+def function_info(func: Callable[..., Any]) -> Callable[..., Any]:
+    func_info = get_function_info(func)
+    func.json_info = func_info
+    return func
