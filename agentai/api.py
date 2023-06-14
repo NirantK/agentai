9e95a24a3cefa6934e2b3a0440d14d79f4d639d8
NirantK <nirant.bits@gmail.com>
2023-06-14 15:54:57 +0530

    * feat(api.py): add debug flag to chat_completion_request function to print debug information * fix(api.py): raise ValueError if messages is not a list of strings

--- agentai/api.py
+++ agentai/api.py
@@ -1,18 +1,28 @@
-# api.py
+"""
+API functions for the agentai package
+"""
 import openai
 import requests
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 
 
 @retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(3))
-def chat_completion_request(messages, model, functions=None):
+def chat_completion_request(messages, model, functions=None, debug=False):
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer " + openai.api_key,
     }
+    if debug:
+        print("Generating ChatCompletion response")
+        print(f"messages: {messages}")
+    if not len(messages) > 0:
+        raise ValueError("messages must be a list of strings")
     json_data = {"model": model, "messages": messages}
     if functions is not None:
         json_data.update({"functions": functions})
+        if debug:
+            print(f"functions: {functions}")
+
     try:
         response = requests.post(
             "https://api.openai.com/v1/chat/completions",
