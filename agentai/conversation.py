c60386bda40829f364ee0c768b8cf30f89eea844
adivik2000 <adivik2000@hotmail.com>
2023-08-01 22:27:03 +0530

    merged conversation history keeping the style of original + docs

--- agentai/conversation.py
+++ agentai/conversation.py
@@ -1,5 +1,7 @@
+import threading
 from typing import List, Optional
 
+import tiktoken
 from pydantic import BaseModel
 from termcolor import colored
 
@@ -11,7 +13,7 @@ class Message(BaseModel):
 
 
 class Conversation:
-    def __init__(self, history: List[Message] = [], id: Optional[str] = None):
+    def __init__(self, history: List[Message] = [], id: Optional[str] = None, max_history_length: int = 20):
         self.history: List[Message] = history
         self.role_to_color = {
             "system": "red",
@@ -20,19 +22,55 @@ class Conversation:
             "function": "magenta",
         }
         self.id = id
+        self.max_history_length = max_history_length
 
-    def add_message(self, role: str, content: str, name: Optional[str] = None):
+    def add_message(self, role: str, content: str, name: Optional[str] = None) -> None:
         message_dict = {"role": role, "content": content}
-        if name is not None:
+        if name:
             message_dict["name"] = name
         message = Message(**message_dict)
         self.history.append(message)
+        self.trim_history()
 
-    def display_conversation(self):
+    def display_conversation(self) -> None:
         for message in self.history:
             print(
                 colored(
                     f"{message.role}: {message.content}\n\n",
-                    self.role_to_color[message.role],
+                    self.role_to_color[message.role.lower()],
                 )
             )
+
+    def trim_history(self) -> None:
+        "Function to keep the message history from growing too large"
+        if len(self.history) > self.max_history_length:
+            self.history = self.history[-self.max_history_length :]
+
+    def trim_history_by_tokens(self, max_tokens: int) -> None:
+        """Function to keep the message history based on the number of tokens
+        This is important to keep the message history crisp to not flood over the
+        max_tokens limit of the AI models and give lee-way to the user prompt
+        """
+        local = threading.local()
+        try:
+            enc = local.gpt2enc
+        except AttributeError:
+            enc = tiktoken.get_encoding("gpt2")
+            local.gpt2enc = enc
+
+        total_tokens = 0
+        for i in range(len(self.history) - 1, -1, -1):
+            # Iterate over the messages in reverse order - from the latest to the oldest messages
+            message = self.history[i]  # Message(role='User', content='I appreciate that. Take care too!', name=None)
+            content = message.content
+            tokens = len(enc.encode(content))
+            total_tokens += tokens
+            if total_tokens > max_tokens:
+                # Trim the history inplace to keep the total tokens under max_tokens
+                self.history = self.history[i + 1 :]
+                break
+
+    def get_history(self, max_tokens=100) -> List[Message]:
+        """Function to get the conversation history based on the number of tokens"""
+        self.trim_history_by_tokens(max_tokens=max_tokens)
+        return self.history
--- docs/test_conversation_history.py
+++ docs/test_conversation_history.py
@@ -1,94 +1,51 @@
-import threading
-import tiktoken
+import os
+import sys
 
+parent1_path = os.path.abspath('..')  # Go one level up from the current notebook
+sys.path.append(parent1_path)
 
-class Conversation:
-    def __init__(self, max_history_length=20):
-        "Function to initialize the conversation history"
-        self.max_history_length = max_history_length
-        self.messages = []
-
-    def add_message(self, sender, content):
-        "Function to add a message to the conversation history"
-        self.messages.append({"sender": sender, "content": content})
-        self.trim_history()
-
-    def trim_history(self):
-        "Function to keep the message history from growing too large - Optional"
-        if len(self.messages) > self.max_history_length:
-            # Slice the list inplace to the last max_history_length messages
-            self.messages = self.messages[-self.max_history_length :]
-
-    def trim_history_by_tokens(self, max_tokens):
-        """Function to keep the message history based on the number of tokens
-        This is important to keep the message history crisp to not flood over the
-        max_tokens limit of the AI models and give lee-way to the user prompt
-        """
-        local = threading.local()
-        try:
-            enc = local.gpt2enc
-        except AttributeError:
-            enc = tiktoken.get_encoding("gpt2")
-            local.gpt2enc = enc
-
-        total_tokens = 0
-        for i in range(len(self.messages) - 1, -1, -1):
-            # Iterate over the messages in reverse order - from the latest to the oldest messages
-            message = self.messages[i]  # Dict {"sender": sender, "content": content}
-            content = message["content"]
-            tokens = len(enc.encode(content))
-            total_tokens += tokens
-            if total_tokens > max_tokens:
-                # Trim the history inplace to keep the total tokens under max_tokens
-                self.messages = self.messages[i + 1 :]
-                break
-
-    def get_history(self, max_tokens=100):
-        """Function to get the conversation history based on the number of tokens"""
-        self.trim_history_by_tokens(max_tokens=max_tokens)
-        return self.messages
-
+from agentai.conversation import Conversation
 
 # Create a conversation object with a maximum history length of 20 messages
 conversation = Conversation(max_history_length=20)
 
 # Chatgpt Simulated conversations of different length
 conversation.add_message("User", "Hello!")
-conversation.add_message("AI", "Hi there!")
+conversation.add_message("Assistant", "Hi there!")
 conversation.add_message("User", "How are you?")
-conversation.add_message("AI", "I'm doing well, thank you!")
+conversation.add_message("Assistant", "I'm doing well, thank you!")
 conversation.add_message("User", "That's great to hear!")
-conversation.add_message("AI", "Thank you! How can I assist you today?")
+conversation.add_message("Assistant", "Thank you! How can I assist you today?")
 conversation.add_message("User", "I have a recipe conundrum. I want to make something special for dinner.")
-conversation.add_message("AI", "That sounds fun! What type of cuisine are you interested in?")
+conversation.add_message("Assistant", "That sounds fun! What type of cuisine are you interested in?")
 conversation.add_message("User", "I'd like to try making a Thai dish.")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "Excellent choice! How about trying to make Pad Thai? It's a popular Thai noodle dish.",
 )
 conversation.add_message("User", "Pad Thai sounds delicious! Do you have a recipe?")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "Certainly! Here's a simple Pad Thai recipe for you:\n\nIngredients:\n- Rice noodles\n- Tofu or your choice of protein\n- Eggs\n- Bean sprouts\n- Chopped green onions\n- Chopped peanuts\n- Lime wedges\n- Pad Thai sauce (made from tamarind paste, fish sauce, sugar, and chili flakes)\n\nInstructions:\n1. Soak the rice noodles in warm water until they soften. Drain and set aside.\n2. In a wok or large pan, stir-fry the tofu or protein until cooked. Push it to one side of the pan.\n3. Crack the eggs into the pan and scramble them until they're nearly cooked.\n4. Add the softened rice noodles to the pan and pour the Pad Thai sauce over them.\n5. Toss everything together until the noodles are coated in the sauce.\n6. Add bean sprouts and chopped green onions, continuing to stir-fry until they're tender.\n7. Serve hot, garnished with chopped peanuts and lime wedges.\n\nEnjoy your Pad Thai!",
 )
 conversation.add_message("User", "Wow, thank you! I can't wait to try making it.")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "You're welcome! Cooking can be a delightful experience. If you have any more questions or need assistance with anything else, feel free to ask.",
 )
 conversation.add_message("User", "Sure thing! By the way, what's the weather like today?")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "I apologize, but as an AI language model, I don't have access to real-time data, including weather updates. You can check the weather forecast on a reliable website or a weather app for accurate information.",
 )
 conversation.add_message("User", "No problem. I have another question then. Can you tell me a joke?")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "Of course! Here's one for you: Why don't scientists trust atoms? Because they make up everything!",
 )
 conversation.add_message("User", "Haha, that's a good one! I needed that laugh. Thanks!")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "You're welcome! Laughter is the best medicine. If you ever need more jokes or anything else, I'm here to brighten your day!",
 )
 conversation.add_message(
@@ -96,22 +53,23 @@ conversation.add_message(
     "That's very kind of you. Actually, I've been feeling a bit stressed lately.",
 )
 conversation.add_message(
-    "AI",
+    "Assistant",
     "I'm sorry to hear that. Stress can be challenging to deal with. Is there something specific bothering you, or do you need some general tips to manage stress?",
 )
 conversation.add_message("User", "I'd appreciate some tips on how to manage stress.")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "Of course! Here are some helpful tips:\n1. Practice deep breathing or meditation to relax your mind and body.\n2. Engage in physical activities like walking, jogging, or yoga to release endorphins.\n3. Take breaks and do something you enjoy, like reading, listening to music, or spending time in nature.\n4. Prioritize your tasks and break them into smaller, manageable steps.\n5. Reach out to friends or family for support and talk about what's on your mind.\n6. Get enough sleep to allow your body to recover and rejuvenate.\n7. Consider trying relaxation techniques like progressive muscle relaxation or visualization.\n8. Limit caffeine and alcohol consumption, as they can increase stress levels.\n9. Learn to say no to tasks or commitments when you feel overwhelmed.\n10. Seek professional help if you find that stress is significantly affecting your daily life.\n\nRemember, it's essential to take care of yourself, and don't hesitate to seek support when needed.",
 )
 conversation.add_message("User", "Thank you for the wonderful advice. I'll definitely give these tips a try.")
 conversation.add_message(
-    "AI",
+    "Assistant",
     "You're welcome! I hope these tips help you find some relief from stress. If you have any more questions or need further assistance, don't hesitate to ask. Take care!",
 )
 conversation.add_message("User", "I appreciate that. Take care too!")
-conversation.add_message("AI", "Thank you! Have a fantastic day!")
+conversation.add_message("Assistant", "Thank you! Have a fantastic day!")
 
 # Get the current conversation history with a maximum token limit of 500
 history = conversation.get_history(max_tokens=500)
+conversation.display_conversation()
 print(history)
