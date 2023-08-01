ec51dddb6d9ff1295d5a7ca917be694582787783
adivik2000 <adivik2000@hotmail.com>
2023-08-01 14:12:20 +0530

    markdown chunking with langchain's md header splitter and tiktoken text splitter

---
+++ agentai/markdown_chunking.py
@@ -0,0 +1,21 @@
+from langchain.text_splitter import MarkdownHeaderTextSplitter
+from langchain.text_splitter import TokenTextSplitter
+
+markdown_document = "# Intro \n\n    ## History \n\n Markdown[9] is a lightweight markup language for creating formatted text using a plain-text editor. John Gruber created Markdown in 2004 as a markup language that is appealing to human readers in its source code form.[9] \n\n Markdown is widely used in blogging, instant messaging, online forums, collaborative software, documentation pages, and readme files. \n\n ## Rise and divergence \n\n As Markdown popularity grew rapidly, many Markdown implementations appeared, driven mostly by the need for \n\n additional features such as tables, footnotes, definition lists,[note 1] and Markdown inside HTML blocks. \n\n #### Standardization \n\n From 2012, a group of people, including Jeff Atwood and John MacFarlane, launched what Atwood characterised as a standardisation effort. \n\n ## Implementations \n\n Implementations of Markdown are available for over a dozen programming languages."
+
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+    ("####", "Header 4"),
+]
+
+# Step 1: Split the markdown document into sections
+# MD splits - into sections
+markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+md_header_splits = markdown_splitter.split_text(markdown_document)
+
+# Step 2: Split the sections into chunks of text using a tiktoken splitter
+text_splitter = TokenTextSplitter(chunk_size=100, chunk_overlap=10)
+chunks = text_splitter.split_documents(md_header_splits)
+print(chunks)
