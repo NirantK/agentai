5a29dc66db84f64b7b0352a8bc6a20bff07895e2
adivik2000 <adivik2000@hotmail.com>
2023-08-14 18:35:16 +0530

    vectordb initial push

--- agentai/parsers.py
+++ agentai/parsers.py
@@ -1,41 +1,43 @@
-from unstructured.partition.pdf import partition_pdf
 from typing import List
 
-from langchain.docstore.document import Document
 import pandas as pd
+from langchain.docstore.document import Document
+from unstructured.partition.pdf import partition_pdf
 
 
-def process_element(element):
-    metadata = {}
-    if hasattr(element, "metadata"):
-        # metadata.update(element.metadata.to_dict())
-        if hasattr(element.metadata, "page_number"):
-            metadata["page_number"] = element.metadata.page_number
+class Parser:
+    def __init__(self):
+        self.filename = ""
+        self.docs: List[Document] = list()
 
-        # default metadata by unstructured
-        metadata["filename"] = element.metadata.filename
-        metadata["filetype"] = element.metadata.filetype
+    def process_element(self, element):
+        metadata = {}
+        if hasattr(element, "metadata"):
+            # metadata.update(element.metadata.to_dict())
+            if hasattr(element.metadata, "page_number"):
+                metadata["page_number"] = element.metadata.page_number
 
-    if hasattr(element, "category"):
-        metadata["category"] = element.category
-        if element.category == "Table":
-            # metadata["table"] = element.metadata.to_dict()
-            table_uno_list = pd.read_html(element.metadata.text_as_html)
-            for _, row in table_uno_list[0].iterrows():
-                # go through each row of the table and create a document
-                # with the row data dictionary as page content
-                page_content = row.to_dict()
-                docs.append(Document(page_content=str(page_content), metadata=metadata))
-        else:
-            # if element is not a table, then it is a text
-            docs.append(Document(page_content=str(element), metadata=metadata))
+            # default metadata by unstructured
+            metadata["filename"] = element.metadata.filename
+            metadata["filetype"] = element.metadata.filetype
 
+        if hasattr(element, "category"):
+            metadata["category"] = element.category
+            if element.category == "Table":
+                # metadata["table"] = element.metadata.to_dict()
+                table_uno_list = pd.read_html(element.metadata.text_as_html)
+                for _, row in table_uno_list[0].iterrows():
+                    # go through each row of the table and create a document
+                    # with the row data dictionary as page content
+                    page_content = row.to_dict()
+                    self.docs.append(Document(page_content=str(page_content), metadata=metadata))
+            else:
+                # if element is not a table, then it is a text
+                self.docs.append(Document(page_content=str(element), metadata=metadata))
 
-filename = "layout-parser-paper.pdf"
-elements = partition_pdf(filename=filename, infer_table_structure=True)
-docs: List[Document] = list()
-
-for element in elements:
-    process_element(element)
-
-print(docs)
+    def parse_pdf(self, filename: str):
+        self.filename = filename
+        elements = partition_pdf(self.filename)
+        for element in elements:
+            self.process_element(element)
+        return self.docs
---
+++ agentai/vectordb.py
@@ -0,0 +1,39 @@
+import chromadb
+from parsers import Parser
+import uuid
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from agentai.annotations import tool, ToolRegistry
+
+
+class Query(BaseModel):
+    query_embedding: List[int] = Field(..., description="Embedding for the query to srach")
+    query_text: Optional[str] = Field(..., description="Simplified query from the user to search")
+    k: str = Field(..., description="The number of results requested")
+
+
+class VectorDB(BaseModel):
+    self.client = None
+
+
+class ChromaDB(VectorDB):
+    def __init__(self):
+        self.client = chromadb.Client()
+        self.collection = self.client.create_collection(name="my_collection")
+
+    def doc_loader(self, filename: str):
+        parser = Parser()
+        docs = parser.parse_pdf(filename)
+        # Using chroma db as an example
+        for doc in docs:
+            self.collection.add(
+                documents=doc.page_content,
+                metadatas=doc.metadata,
+                ids=str(uuid.uuid4()),
+            )
+
+    def get_docs(self, query: Query):
+        results = self.collection.query(
+            query_embeddings=[query.query_embedding], query_texts=[query.query_text], n_results=query.k
+        )
+        return results
